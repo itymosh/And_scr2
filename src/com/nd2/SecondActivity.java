0b2fbff255a89a2f2624af49db98c78ab0826dc3
itymosh <ihortymoshenko@gmail.com>
2015-07-22 11:20:59 +0300

    right/left swipe work

--- src/com/nd2/SecondActivity.java
+++ src/com/nd2/SecondActivity.java
@@ -6,6 +6,7 @@ import java.util.Random;
 
 
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.ClipData;
 import android.content.ClipDescription;
@@ -33,13 +34,14 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+@SuppressLint("ClickableViewAccessibility")
 public class SecondActivity extends Activity implements OnClickListener{
 	 private static final int SWIPE_MIN_DISTANCE = 120;
 	    private static final int SWIPE_MAX_OFF_PATH = 250;
 	    private static final int SWIPE_THRESHOLD_VELOCITY = 200;
 	    private GestureDetector gestureDetector;
 	    View.OnTouchListener gestureListener;
-		private ImageView myImage;
+	//	private ImageView myImage;
 		private static final String IMAGEVIEW_TAG = "The Android Logo";
 		
 	@Override
@@ -62,108 +64,23 @@ public class SecondActivity extends Activity implements OnClickListener{
 	       findViewById(R.id.activitysecond).setOnClickListener(this); 
 	        findViewById(R.id.activitysecond).setOnTouchListener(gestureListener);
 		
-	        myImage = (ImageView)findViewById(R.id.image);
+	  //      myImage = (ImageView)findViewById(R.id.image);
 		    // Sets the tag
-		    myImage.setTag(IMAGEVIEW_TAG);
+	//	    myImage.setTag(IMAGEVIEW_TAG);
 		    
 		    // set the listener to the dragging data
-		    myImage.setOnLongClickListener(new MyClickListener());
+	//	    myImage.setOnLongClickListener(new MyClickListener());
 		   
-		    findViewById(R.id.toplinear).setOnDragListener(new MyDragListener());
-		    findViewById(R.id.bottomlinear).setOnDragListener(new MyDragListener());
+	//	    findViewById(R.id.toplinear).setOnDragListener(new MyDragListener());
+	//	    findViewById(R.id.bottomlinear).setOnDragListener(new MyDragListener());
 		
 	        
 	
 	}
 	
-	private final class MyClickListener implements OnLongClickListener {
+	
 
-	    // called when the item is long-clicked
-		@Override
-		public boolean onLongClick(View view) {
-		// TODO Auto-generated method stub
-		
-			// create it from the object's tag
-			ClipData.Item item = new ClipData.Item((CharSequence)view.getTag());
-
-	        String[] mimeTypes = { ClipDescription.MIMETYPE_TEXT_PLAIN };
-	        ClipData data = new ClipData(view.getTag().toString(), mimeTypes, item);
-	        DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
-	   
-	        view.startDrag( data, //data to be dragged
-	        				shadowBuilder, //drag shadow
-	        				view, //local data about the drag and drop operation
-	        				0   //no needed flags
-	        			  );
-	        
-	        
-	        view.setVisibility(View.INVISIBLE);
-	        return true;
-		}	
-	}
-
-	class MyDragListener implements OnDragListener {
-		Drawable normalShape = getResources().getDrawable(R.drawable.normal_shape);
-		Drawable targetShape = getResources().getDrawable(R.drawable.target_shape);
-
-		@Override
-		public boolean onDrag(View v, DragEvent event) {
-	  
-			// Handles each of the expected events
-		    switch (event.getAction()) {
-		    
-		    //signal for the start of a drag and drop operation.
-		    case DragEvent.ACTION_DRAG_STARTED:
-		        // do nothing
-		        break;
-		        
-		    //the drag point has entered the bounding box of the View
-		    case DragEvent.ACTION_DRAG_ENTERED:
-		        v.setBackground(targetShape);	//change the shape of the view
-		        break;
-		        
-		    //the user has moved the drag shadow outside the bounding box of the View
-		    case DragEvent.ACTION_DRAG_EXITED:
-		        v.setBackground(normalShape);	//change the shape of the view back to normal
-		        break;
-		        
-		    //drag shadow has been released,the drag point is within the bounding box of the View
-		    case DragEvent.ACTION_DROP:
-		        // if the view is the bottomlinear, we accept the drag item
-		    	  if(v == findViewById(R.id.imageView10)) {
-		    		  View view = (View) event.getLocalState();
-		    		  ViewGroup viewgroup = (ViewGroup) view.getParent();
-		    		  viewgroup.removeView(view);
-		        
-		    		  //change the text
-		    		  TextView text = (TextView) v.findViewById(R.id.text);
-		    		  text.setText("The item is dropped");
-		    		  ImageView l =(ImageView) findViewById(R.id.imageView10);
-		    		  l.setImageResource(R.drawable.ic_launcher);
-		    		  GridLayout containView = (GridLayout) v;
-		    		  containView.addView(view);
-		    		  
-		    		  view.setVisibility(View.VISIBLE);
-		    	  } else {
-		    		  View view = (View) event.getLocalState();
-		    		  view.setVisibility(View.VISIBLE);
-		    		  Context context = getApplicationContext();
-		    		  Toast.makeText(context, "You can't drop the image here", 
-                                                 Toast.LENGTH_LONG).show();
-		    		  break;
-		    	   }
-		    	  break;
-		    	  
-		    //the drag and drop operation has concluded.
-		    case DragEvent.ACTION_DRAG_ENDED:
-		        v.setBackground(normalShape);	//go back to normal shape
-		    
-		    default:
-		        break;
-		    }
-		    return true;
-		}
-	}
+	
 
 	 class MyGestureDetector extends SimpleOnGestureListener {
 	    	private Bundle b;
